0891c58af68c474abceec857a54c8999a9d63239
piyush.nanwani <pnanwani61@gmail.com>
2020-04-18 08:21:28 +0530

    added create api to create an issue,save it to server and use updated returned from server

--- src/App.jsx
+++ src/App.jsx
@@ -111,10 +111,20 @@ class IssueList extends React.Component {
     
     createIssue(newIssue)
     {
-        const newIssues = this.state.issues.slice();
-        newIssue.id = this.state.issues.length + 1;
-        newIssues.push(newIssue);
-        this.setState({issues: newIssues});
+        fetch('/api/issues', {
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify(newIssue), 
+        }).then(response => response.json()
+        ).then(updatedIssue => {
+            updatedIssue.created = new Date(updatedIssue.created);
+            if(updatedIssue.completionDate)
+                updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+            const newIssues = this.state.issues.concat(updatedIssue);
+            this.setState({issues:newIssues});
+        }).catch(err => {
+            alert("Error in sending data to server: "+ err.message);
+        });
     }
 
     render() {
--- static/App.js
+++ static/App.js
@@ -224,10 +224,22 @@ var IssueList = function (_React$Component3) {
     }, {
         key: 'createIssue',
         value: function createIssue(newIssue) {
-            var newIssues = this.state.issues.slice();
-            newIssue.id = this.state.issues.length + 1;
-            newIssues.push(newIssue);
-            this.setState({ issues: newIssues });
+            var _this5 = this;
+
+            fetch('/api/issues', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newIssue)
+            }).then(function (response) {
+                return response.json();
+            }).then(function (updatedIssue) {
+                updatedIssue.created = new Date(updatedIssue.created);
+                if (updatedIssue.completionDate) updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+                var newIssues = _this5.state.issues.concat(updatedIssue);
+                _this5.setState({ issues: newIssues });
+            }).catch(function (err) {
+                alert("Error in sending data to server: " + err.message);
+            });
         }
     }, {
         key: 'render',
