3afbba6d654236aced6f2fff1dd242bff99a0053
piyush.nanwani <pnanwani61@gmail.com>
2020-05-26 09:18:27 +0530

    added code to process DELETE issue request

--- server/server.js
+++ server/server.js
@@ -35,6 +35,10 @@ app.get('/api/issues', (req,res)=> {
         res.status(500).json({message: `Internal Server Error: ${error}`});
     });
 });
+app.get('/api/issues/:id', (req, res)=> {
+    let issueId = req.params.id;
+    console.log("Hey!! Request recieved " + issueId);
+})
 
 
 app.post('/api/issues', (req,res)=> {
@@ -57,4 +61,23 @@ app.post('/api/issues', (req,res)=> {
             console.log(error);
             res.status(500, json({message: `Internal Server Error: ${error}` } ));
         });
+});
+app.delete('/api/issues/:id', (req,res)=> {
+    let issueId = new String(req.params.id) ;
+    try{
+        var ObjectId = require('mongodb').ObjectID;
+        issueId = new ObjectId(req.params.id);
+    } catch(error) {
+        res.status(422).json({message : `Invalid issue ID format: ${error}`});
+        return;
+    }
+
+    db.collection('issues').deleteOne({_id: issueId}).then((delteResult)=> {
+        if(delteResult.result.n === 1) res.json({ status: 'OK'});
+        else res.json({status: 'Warning: object not found'});
+    })
+    .catch(error=> {
+        console.log(error);
+        res.status(500).json({message: `Internal Server Error : $(error)`});
+    });
 });
