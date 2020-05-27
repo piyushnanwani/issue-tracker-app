766d77f57d1ca38c65d5a20365e39b9b7dddd1c2
piyush.nanwani <pnanwani61@gmail.com>
2020-05-27 08:45:36 +0530

    added form to edit issue

--- src/IssueEdit.jsx
+++ src/IssueEdit.jsx
@@ -4,20 +4,85 @@ import PropTypes from 'prop-types';
 
 export default class IssueEdit extends React.Component
 { 
+    constructor(){
+        super();
+        this.state = {
+            issue: {
+                _id: '', title:'', status: '', owner:'', effort:'',
+                completionDate:'', created:''
+            },
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps.match.params.id != this.props.match.params.id){
+            this.loadData();
+        }
+    }
+    onChange(event){
+        const issue = Object.assign({}, this.state.issue);
+        issue[event.target.name] = event.target.value;
+        this.setState({issue});
+    }
+    loadData() {
+
+        fetch(`/api/issues/${this.props.match.params.id}`).then(response => {
+            if(response.ok){
+                response.json().then(issue => {
+                    issue.created = new Date(issue.created).toString();
+                    issue.completionDate = issue.completionDate != null ? new Date(issue.completionDate):
+                        '';
+                    issue.effort = issue.effort !=null ? issue.effort.toString() : '';
+                    this.setState(issue);
+                });
+            } else {
+                response.json().then(error=> {
+                    alert(`Failed to fetch issue: ${error.message}`);
+                });
+            }
+        }). catch(err=>{
+            alert(`Error in fetching data from server: ${err.message}`);
+        });
+    }
+
     render(){
+        const issue = this.state.issue;
         return (
-            <div> 
-                <p>
-                This is a placeholder for the editing issue
-                <b> {this.props.match.params.id}</b>
-                </p>
-                <Link to="/issues">back to issue list</Link>
+            <div>
+                <form>
+                    ID: {issue._id}
+                    <br/>
+                    Created: {issue.created}
+                    <br/>
+                    Status: <select name="status" value={issue.status} onChange={this.onChange}>
+                        <option value="New">New</option>
+                        <option value="Open">Open</option>
+                        <option value="Assigned">Assigned</option>
+                        <option value="Fixed">Fixed</option>
+                        <option value="Verified">Verified</option>
+                        <option value="Closed">Closed</option>
+                    </select>
+                    <br/>
+                    Owner: <input name="owner" value={issue.owner} onChange={this.onChange}/>
+                    <br/>
+                    Effort: <input size={5} name="effort" value={issue.effort} onChange={this.onChange} />
+                    <br/>
+                    Title: <input name="title" size={50} value={issue.title} onChange={this.onChange} />
+                    <br/>
+                    <button type="submit">Submit</button>
+                    <Link to="/issues"> Back to issue list</Link>
+                </form>
             </div>
-        );
+        )
     }
 
 }
 
 IssueEdit.propTypes = {
-    id: PropTypes.object,
+    params: PropTypes.object,
 };
